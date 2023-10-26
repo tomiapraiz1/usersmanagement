915215b77f7c2afd4760d1a0f24c47f339402109
tomiapraiz1 <tomiapraiz1@gmail.com>
2023-10-26 11:51:05 -0300

    docker compose for db added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, APIRouter
+from app.database import models
+from app.database.database import engine
 from app.routers import users
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 app.include_router(
     router=users.router,
     prefix="/api"
 )
-
 
 @app.get("/")
 async def root():
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,27 +1,27 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from app.database import crud, models, schemas
-from app.database.database import get_db, engine
+from app.database import user_service, schemas
+from app.database.database import get_db
 
 from uuid import UUID
-
-models.Base.metadata.create_all(bind=engine)
 
 router = APIRouter(
     prefix="/users",
     tags=["users"]
 )
 
+db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.get("/", response_model=list[schemas.User])
-async def get_all_users(db: Session = Depends(get_db)):
-    return crud.get_all_users(db)
+async def get_all_users(db: db_dependency):
+    return user_service.get_all_users(db)
 
 
 @router.get("/{user_id}", response_model=schemas.User)
-async def get_user_by_id(user_id: UUID, db: Session = Depends(get_db)):
-    user = crud.get_user_by_id(db, user_id)
+async def get_user_by_id(user_id: UUID, db: db_dependency):
+    user = user_service.get_user_by_id(db, user_id)
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
@@ -29,8 +29,8 @@ async def get_user_by_id(user_id: UUID, db: Session = Depends(get_db)):
 
 
 @router.post("/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    user = crud.create_user(db, user)
+async def create_user(user: schemas.UserCreate, db: db_dependency):
+    user = user_service.create_user(db, user)
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
@@ -38,8 +38,8 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.put("/{user_id}")
-async def update_user(user_id: UUID, user: schemas.UserCreate, db: Session = Depends(get_db)):
-    user_updated = crud.update_user(db, user_id, user)
+async def update_user(user_id: UUID, user: schemas.UserCreate, db: db_dependency):
+    user_updated = user_service.update_user(db, user_id, user)
     if user_updated is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
@@ -47,8 +47,8 @@ async def update_user(user_id: UUID, user: schemas.UserCreate, db: Session = Dep
 
 
 @router.delete("/{user_id}")
-async def delete_user(user_id: UUID, db: Session = Depends(get_db)):
-    user_deleted = crud.delete_user(db, user_id)
+async def delete_user(user_id: UUID, db: db_dependency):
+    user_deleted = user_service.delete_user(db, user_id)
     if user_deleted is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
